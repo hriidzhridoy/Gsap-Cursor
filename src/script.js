f5a4ba9c5a49df313fcc687ce63050f080f8c1c0
hriidzhridoy <you@example.com>
2024-05-04 00:39:07 +0600

    some chnages

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
-var main = document.querySelector("#main");
+// var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
 
 main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
     x: dets.x,
     y: dets.y,
-    rotation: dets.rotation,
-    scale: dets.scale,
+    // rotation: dets.rotation,
+    // scale: dets.scale,
     duration: 0.5,
     ease: "elastic.out(1, 0.3)",
   });
